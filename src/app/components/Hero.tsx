8ee4e550eb3725b673e0482ac820fb62e473e831
Hatem Soliman <hatemthedev@gmail.com>
2024-08-30 15:20:00 +0300

    Added Hero seciton

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default function Hero() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, 
+      offset: 200,
+      easing: 'ease-out',
+    });
+  }, []);
+
+  return (
+    <section className="mt-24">
+      <div className="kontainer text-center">
+        <div className="row text-md sm:text-xl text-white flex flex-col items-center gap-8">
+          <h1 
+            data-aos="fade-up"
+            className="text-2xl text-packship-red font-bold"
+          >
+            Packship CLI Tool
+          </h1>
+          <h2 
+            data-aos="fade-up"
+            className="text-6xl font-black text-black sm:text-8xl leading-relaxed"
+          >
+            Simplify Your <span className="text-packship-red">NPM</span> Package <span className="text-packship-red">Releases</span>
+          </h2>
+          <p
+            data-aos="fade-up"
+            className="text-lg text-packship-red font-semibold my-4"
+          >
+            Ready to streamline your package publishing process? Packship makes shipping npm packages easier and faster.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Footer from "@/shared/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import DonationsLink from "@/shared/DonationsLink";
+import Hero from "./components/Hero";
 
 function useAnimatedNumber(target: number, duration: number) {
   const [currentNumber, setCurrentNumber] = useState<number>(0);
@@ -55,114 +56,7 @@ export default function Home() {
     <Suspense>
       <Navigation />
       <main>
-        <section className="mt-24">
-          <div className="kontainer text-center">
-            <div className="row text-md sm:text-xl text-white flex flex-col items-center gap-32">
-              <h1 
-                data-aos="fade-up"
-                className="text-6xl sm:text-8xl leading-snug"
-              >
-                Next Generation of App <span className="">Previewing</span>
-              </h1>
-              <figure 
-                data-aos="fade-up"
-                className="w-full" 
-              >
-                <Image 
-                  src={""}
-                  alt="iPhone 15 Pro & Google Pixel 8"
-                  className="w-full"
-                />
-              </figure>
-              <button 
-                type="button" 
-                data-aos="fade-up"
-              >
-                <Link
-                  href="/getting-started"
-                  className="bg-white rounded-full text-2xl px-8 py-4"
-                >
-                  Get Started
-                </Link>
-              </button>
-            </div>
-          </div>
-        </section>
-
-        <section data-aos="fade-up">
-          <div className="kontainer text-center">
-            <div className="row text-md sm:text-xl text-white flex flex-col items-center gap-32">
-              <div 
-                data-aos="fade-up"
-                className="w-full flex flex-col items-center" 
-              >
-                <figure className="w-full sm:w-1/2">
-                  <Image
-                    src={""}
-                    alt="InstallationPrompt"
-                    className="w-full"
-                  />
-                </figure>
-                <div className="bg-framify-silver rounded-b-3xl rounded-t-lg sm:rounded-full text-white w-full sm:w-10/12 px-8 sm:px-24 py-8 flex flex-col items-center gap-8">
-                  <blockquote className="text-2xl leading-relaxed sm:leading-none">
-                    Software development isn&apos;t just about &ldquo;getting the task done&rdquo; it&apos;s about the creativity involved in bringing that idea to life.
-                  </blockquote>
-                  <div className="bg-framif rounded-3xl sm:rounded-full px-8 py-4 flex flex-col sm:flex-row justify-between items-center gap-8">
-                    <figure className="w-20">
-                      <Image 
-                        src={""}
-                        alt="Hatem Soliman"
-                        className="w-full rounded-full"
-                      />
-                    </figure>
-                    <cite className="flex flex-col text-center sm:text-start gap-1">
-                      <h3 className="text-sm">Hatem Soliman</h3>
-                      <h4 className="text-sm">Creator of React Framify</h4>
-                    </cite>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <h2 className="text-4xl text-center mt-56 mb-8 leading-relaxed sm:leading-none">Join {animatedNumber.toLocaleString()}+ exciting and anticipated projects</h2>
-                <span className="bg-framify-black text-sm text-center p-2">
-                  Data sourced from &nbsp;
-                  <Link
-                    href="https://npmtrends.com"
-                    className=""
-                  >
-                    npmtrends.com
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="bg-white w-full mt-16 py-4 ribbon-container">
-            <div className="ribbon-content">
-              {Array(isMobile ? 1 : 3).fill(null).map((_, repeatIndex) => (
-                <div 
-                  key={repeatIndex} 
-                  className="flex"
-                >
-                  {brands.map((brand, index) => (
-                    <div key={index} className="flex-shrink-0">
-                      <Image
-                        src={brand.img}
-                        alt={brand.title}
-                        width={200}
-                        height={24}
-                      />
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="kontainer text-center">
-            <div className="row text-md sm:text-xl text-white flex flex-col items-center">
-              <DonationsLink />
-            </div>
-          </div>
-        </section>
+        <Hero />
       </main>
       <Footer />
     </Suspense>
