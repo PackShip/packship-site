c17eb8698f2e5afd06ee7cd91911c2a1a3b266c1
Hatem Soliman <hatemthedev@gmail.com>
2024-09-20 23:31:06 +0300

    Added Loom walkthrough

--- src/app/components/Walkthrough.tsx
+++ src/app/components/Walkthrough.tsx
@@ -3,7 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import SectionHeader from "@/shared/SectionHeader";
 
-export default function Preview() {
+export default function Walkthrough() {
 
   useEffect(() => {
     AOS.init({
@@ -17,9 +17,9 @@ export default function Preview() {
     <section className="mt-24">
       <div className="kontainer text-center">
         <div className="row text-md sm:text-xl text-white flex flex-col items-center gap-8">
-          <SectionHeader header="Promo Launch Trailer | Skit" />
+          <SectionHeader header="Walkthrough" />
           <div data-aos="fade-up" className="w-full flex flex-col justify-between items-center">
-            <iframe src="https://cards.producthunt.com/cards/products/607718" style={{ width: '100%', maxWidth: '500px', height: '405px' }} className="bg-white bg-opacity-75 rounded-xl p-2 md:w-500" />
+            <iframe src="https://www.loom.com/embed/c603f4de707e424ab45deff1f6fe9f09?sid=8c9962de-7081-4ac7-9910-781ef94d4052"  style={{ width: '100%', maxWidth: '500px', height: '405px' }} className="bg-white bg-opacity-75 rounded-xl p-2 md:w-500" />
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Features from "./components/Features";
 import Instructions from "./components/Instructions";
 import Pricing from "./components/Pricing";
 import Preview from "./components/Preview";
+import Walkthrough from "./components/Walkthrough";
 
 function useAnimatedNumber(target: number, duration: number) {
   const [currentNumber, setCurrentNumber] = useState<number>(0);
@@ -64,6 +65,7 @@ export default function Home() {
         <Features />
         <Preview />
         <Instructions />
+        <Walkthrough />
         <Pricing />
       </main>
       <Footer />
