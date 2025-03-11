96ad22e06d32aaf09c0198960eb509994b89b3ab
Hatem Soliman <hatemthedev@gmail.com>
2025-03-12 01:19:26 +0200

    Removed redundant dpednacy

--- src/app/references/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function References() {
-  redirect("/packship-pug");
-}
--- src/shared/ClientProviders.tsx
+++ src/shared/ClientProviders.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Provider } from "react-redux";
-import { store } from "@/redux/store";
 import { ReactNode } from "react";
 import { ThemeProvider } from "@/context/ThemeContext";
 
@@ -10,9 +8,5 @@ interface ClientProvidersProps {
 }
 
 export default function ClientProviders({ children }: ClientProvidersProps) {
-  return (
-    <Provider store={store}>
-      <ThemeProvider>{children}</ThemeProvider>
-    </Provider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
