d88a471de3b558ff260e0654cfe1d5d12172ccb0
Hatem Soliman <hatemthedev@gmail.com>
2025-03-11 20:06:07 +0200

    Updated init cmd page

--- src/app/docs/cli-init/page.tsx
+++ src/app/docs/cli-init/page.tsx
@@ -38,6 +38,14 @@ export default function CliInit() {
         </div>
 
         <DocParagraph>
+          If you haven&apos;t installed PackShip globally, you can use npx:
+        </DocParagraph>
+
+        <div className="mb-4">
+          <CopyCodeSnippet code="npx packship init" language="bash" />
+        </div>
+
+        <DocParagraph>
           This will start an interactive setup process that guides you through
           configuring your package.
         </DocParagraph>
@@ -45,56 +53,8 @@ export default function CliInit() {
         <DocH2 id="command-options">Command Options</DocH2>
 
         <DocParagraph>
-          The <DocCode>init</DocCode> command supports several options to
-          customize the initialization process:
+          The <DocCode>init</DocCode> command supports the following option:
         </DocParagraph>
-
-        <DocH3>--template [name]</DocH3>
-
-        <DocParagraph>
-          Specify a template to use for your package. Available templates
-          include:
-        </DocParagraph>
-
-        <DocList>
-          <DocListItem>
-            <DocCode>react</DocCode>: For React component libraries (default)
-          </DocListItem>
-          <DocListItem>
-            <DocCode>vanilla</DocCode>: For plain JavaScript/TypeScript
-            libraries
-          </DocListItem>
-          <DocListItem>
-            <DocCode>node</DocCode>: For Node.js packages
-          </DocListItem>
-        </DocList>
-
-        <div className="mb-4">
-          <CopyCodeSnippet
-            code="packship init --template vanilla"
-            language="bash"
-          />
-        </div>
-
-        <DocH3>--typescript</DocH3>
-
-        <DocParagraph>
-          Initialize the project with TypeScript support (this is the default).
-        </DocParagraph>
-
-        <div className="mb-4">
-          <CopyCodeSnippet code="packship init --typescript" language="bash" />
-        </div>
-
-        <DocH3>--javascript</DocH3>
-
-        <DocParagraph>
-          Initialize the project with JavaScript instead of TypeScript.
-        </DocParagraph>
-
-        <div className="mb-4">
-          <CopyCodeSnippet code="packship init --javascript" language="bash" />
-        </div>
 
         <DocH3>--yes, -y</DocH3>
 
@@ -102,6 +62,12 @@ export default function CliInit() {
 
         <div className="mb-4">
           <CopyCodeSnippet code="packship init -y" language="bash" />
+        </div>
+
+        <DocParagraph>Or with npx:</DocParagraph>
+
+        <div className="mb-4">
+          <CopyCodeSnippet code="npx packship init -y" language="bash" />
         </div>
 
         <DocNote>
@@ -114,58 +80,22 @@ export default function CliInit() {
 
         <DocParagraph>
           When running <DocCode>packship init</DocCode> without the{" "}
-          <DocCode>-y</DocCode> flag, you&apos;ll be prompted to provide the
-          following information:
-        </DocParagraph>
-
-        <DocH4>Package Name</DocH4>
-        <DocParagraph>
-          The name of your package. This should be unique on npm and follow npm
-          naming conventions.
-        </DocParagraph>
-
-        <DocH4>Package Description</DocH4>
-        <DocParagraph>
-          A brief description of what your package does.
-        </DocParagraph>
-
-        <DocH4>Author</DocH4>
-        <DocParagraph>Your name or organization name.</DocParagraph>
-
-        <DocH4>License</DocH4>
-        <DocParagraph>
-          The license for your package. The default is MIT.
-        </DocParagraph>
-
-        <DocH4>Version</DocH4>
-        <DocParagraph>
-          The initial version of your package. The default is 0.1.0.
+          <DocCode>-y</DocCode> flag, you&apos;ll be prompted to provide package
+          creation information.
         </DocParagraph>
 
         <DocH2 id="examples">Examples</DocH2>
-
-        <DocH3>Creating a React Component Library</DocH3>
-
-        <div className="mb-4">
-          <CopyCodeSnippet
-            code="packship init --template react"
-            language="bash"
-          />
-        </div>
-
-        <DocH3>Creating a JavaScript Utility Library</DocH3>
-
-        <div className="mb-4">
-          <CopyCodeSnippet
-            code="packship init --template vanilla --javascript"
-            language="bash"
-          />
-        </div>
 
         <DocH3>Quick Initialization with Defaults</DocH3>
 
         <div className="mb-4">
           <CopyCodeSnippet code="packship init -y" language="bash" />
+        </div>
+
+        <DocH3>Using npx for One-time Execution</DocH3>
+
+        <div className="mb-4">
+          <CopyCodeSnippet code="npx packship init" language="bash" />
         </div>
 
         {/* Next Page Button */}
