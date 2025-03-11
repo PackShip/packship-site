0949889ccdac25384d2f860727cbf6f4c5325dab
Hatem Soliman <hatemthedev@gmail.com>
2025-03-12 00:10:26 +0200

    Changed icon from copy icon

--- src/shared/CopyCodeSnippet.tsx
+++ src/shared/CopyCodeSnippet.tsx
@@ -74,13 +74,13 @@ export default function CopyCodeSnippet({
 
       <div className="absolute bottom-4 right-4">
         <button
-          className="bg-packship-purple hover:bg-purple-700 text-white font-bold text-sm rounded-full px-4 py-2 transition-colors flex items-center justify-center shadow-lg"
+          className="bg-packship-purple hover:bg-purple-700 text-white font-bold text-sm rounded-full px-3 md:px-4 py-2 transition-colors flex items-center justify-center shadow-lg"
           onClick={copyCodeToClipboard}
           aria-label="Copy code to clipboard"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4 mr-2"
+            className="h-4 w-4 md:mr-2"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -91,7 +91,7 @@ export default function CopyCodeSnippet({
             <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
             <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
           </svg>
-          {buttonText}
+          <span className="hidden md:inline">{buttonText}</span>
         </button>
       </div>
     </div>
